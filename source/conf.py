45ad464b27feb4528fd7d79b7935afca697755d7
smahn <smahn@rosesystems.kr>
2019-08-23 13:27:04 +0900

    latex engine 변경을 위한 시도

--- source/conf.py
+++ source/conf.py
@@ -48,6 +48,11 @@ templates_path = ['_templates']
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
 language = 'ko'
+latex_engine = 'xelatex'
+latex_use_xindy = False
+latex_elements = {
+    'preamble': '\\usepackage[UTF8]{ctex}\n',
+}
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
